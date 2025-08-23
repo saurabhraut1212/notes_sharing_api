203d0f01cd3412d0d554de9e54e14fe956f62e81
saurabhraut1212 <saurabhrmahadev1212@gmail.com>
2025-08-23 09:20:54 +0530

    feat: worked on main file

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/saurabhraut1212/notes_sharing_api/internal/config"
+	"github.com/saurabhraut1212/notes_sharing_api/internal/db"
+	"github.com/saurabhraut1212/notes_sharing_api/internal/router"
+)
 
 func main() {
-	fmt.Println("Notes sharing api")
+
+	cfg := config.Load()
+
+	client, err := db.New(cfg.MongoURI)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	app := router.Setup(client, cfg)
+
+	// Channel to listen for OS signals
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	// Run server in goroutine
+	go func() {
+		log.Printf("🚀 Server is running successfully on http://localhost:%s", cfg.Port)
+		if err := app.Listen(":" + cfg.Port); err != nil {
+			log.Fatal("Failed to start server: ", err)
+		}
+	}()
+
+	// Block until signal is received
+	<-done
+	log.Println("⏳ Shutting down server...")
+
+	// Create a context with timeout to gracefully shutdown
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := app.ShutdownWithContext(ctx); err != nil {
+		log.Fatal("Server forced to shutdown: ", err)
+	}
+
+	log.Println("✅ Server stopped gracefully")
 }
